fa3359faa57ebc5d6b2197173d408f0fb2ca4795
Reuben Frith <reubenfrith007@gmail.com>
2022-04-18 18:56:08 +1000

    created grid using js

---
+++ code.js
@@ -0,0 +1,28 @@
+
+
+const container = document.querySelector('#container');
+
+const grid = document.createElement('div');
+grid.classList.add('grid'); 
+
+
+
+
+grid.setAttribute('style', 'display: grid; grid-template-columns: repeat(4, 20px);');    
+
+
+
+
+for(let i=0; i<16; i++) {
+    var box = document.createElement('div');
+    box.classList.add('box'); 
+    box.setAttribute('style', 'background-color: black; width: 30px ; height: 30px ;');    
+    grid.appendChild(box);
+}
+
+
+
+// place grid inside of #container
+//grid.appendChild(element);
+container.appendChild(grid);
+
