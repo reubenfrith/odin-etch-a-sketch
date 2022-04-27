30e51b7af3cc7aef75beb68a4eb3f401a7930a71
Reuben Frith <reubenfrith007@gmail.com>
2022-04-27 22:50:15 +1000

    working on slider functionality

--- code.js
+++ code.js
@@ -5,9 +5,20 @@ const container = document.querySelector('#container');
 // Create grid
 const grid = document.createElement('div');
 grid.classList.add('grid'); 
-grid.setAttribute('style', 'display: grid; grid-template-columns: repeat(15, 32px);');    
+grid.setAttribute('style', 'display: grid; width:400px ; height: 400px; grid-template-columns: repeat(15 , 1fr);'); 
 
 
+
+/*
+class="slider" 
+*/
+let slider = document.getElementById("dimensions");
+console.log(slider.value);
+
+
+
+
+// rid-template-columns: repeat(30, ${x}px);');
 /*
 Set up intial grid
 - event listener for mousever is made 
@@ -15,19 +26,65 @@ Set up intial grid
 */
 
 for(let i=0; i<225; i++) {
-    var box = document.createElement('div');
+    let box = document.createElement('div');
 
     box.classList.add('box'); 
-    box.setAttribute('style', 'border: 1px solid black; width: 30px ; height: 30px ;');
-
-    box.addEventListener("mouseover", function(e) {
-        // highlight the mouseover target
-        e.target.style.backgroundColor = "orange";
-    })
-
+    box.setAttribute('style', 'border: 1px solid black; width: 24.67px ; height: 24.67px;');
     grid.appendChild(box);
 }
 container.appendChild(grid);
+
+/* Orange Pen 
+- Set up function for orange ped
+- Orange pen is initially loaded on window loading
+*/
+
+const orange_pen = document.querySelector('#orange_pen');
+orange_pen.addEventListener("click", () => orangePen());
+
+window.onload = orangePen();
+
+/* orangePen
+
+onmouse down set variable to true 
+true = background changes
+
+mouseover if variable is true continue to change background
+
+on mouse up set variable to false
+false = no background changing
+
+
+function(e) {
+            e.target.style.backgroundColor = "orange";
+        }
+
+*/
+
+
+
+
+
+function orangePen() {
+
+    let isDrawing = false;
+    let boxes = document.querySelectorAll(".box");
+
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].addEventListener("mousedown", (e) => {
+        e.target.style.backgroundColor = "orange";
+        isDrawing = true; });
+        boxes[i].addEventListener("mouseover", (e) => {
+            if (isDrawing == true) {
+            e.target.style.backgroundColor = "orange";
+            }
+        });
+        boxes[i].addEventListener("mouseup", () => isDrawing = false );
+    }
+
+
+}
+
 
 /*
 Clear All
@@ -36,15 +93,15 @@ Clear All
 - on click select all the boxes  and loop thorugh each box and set its background to white
 */
 const clear = document.querySelector('.clear');
+clear.addEventListener("click", () => clearAll());
 
-clear.addEventListener("click", function() {
+function clearAll() {
 
-    const boxes = document.querySelectorAll(".box");
+    let boxes = document.querySelectorAll(".box");
     for (let i = 0; i < boxes.length; i++) {
       boxes[i].style.backgroundColor = "white";
     }
-
-})
+}    
 
 
 /*
@@ -55,15 +112,28 @@ Eraser
 */
 
 const eraser = document.querySelector('.eraser');
+eraser.addEventListener("click", () => erase());
 
-eraser.addEventListener("click", function() {
+function erase() {
 
-    const boxes = document.querySelectorAll(".box");
+    let isDrawing = false;
+    let boxes = document.querySelectorAll(".box");
+
     for (let i = 0; i < boxes.length; i++) {
-        boxes[i].addEventListener("mouseover", function(e) {
-            // highlight the mouseover target
+        boxes[i].addEventListener("mousedown", (e) => {
+        e.target.style.backgroundColor = "white";
+        isDrawing = true; });
+        boxes[i].addEventListener("mouseover", (e) => {
+            if (isDrawing == true) {
             e.target.style.backgroundColor = "white";
-        })
+            }
+        });
+        boxes[i].addEventListener("mouseup", () => isDrawing = false );
     }
 
-})
+
+}
+
+
+
+
