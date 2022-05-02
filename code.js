f529a58ca91385652e7fe6c633452fd1ac5efbbf
Reuben Frith <reubenfrith007@gmail.com>
2022-05-02 21:53:05 +1000

    all is ready to go , need to make it pretty now

--- code.js
+++ code.js
@@ -1,17 +1,76 @@
 
+
 // Creating container to hold grid and boxes
 const container = document.querySelector('#container');
 
-/*
-class="slider" 
-*/
-let slider = document.getElementById("dimensions").value;
-console.log(slider.value);
-
-// Create grid
 const grid = document.createElement('div');
-grid.classList.add('grid'); 
-grid.setAttribute('style', `display: grid; width:400px ; height: 400px; grid-template-columns: repeat(${slider} , 1fr);`); 
+
+grid.class = 'grid'; 
+grid.id = 'grid';
+
+container.appendChild(grid);
+
+const gridSize = document.getElementById('gridSize');
+
+// Create Initial Grid
+
+let sliderValue = document.getElementById("dimensions").value;
+
+gridSize.textContent = `${sliderValue} x ${sliderValue}`
+
+grid.setAttribute('style', `display: grid; width:400px ; height: 400px; grid-template-columns: repeat(${sliderValue} , 1fr);`); 
+
+for(let i=0; i<(sliderValue*sliderValue); i++) {
+    let box = document.createElement('div');
+
+    box.classList.add('box'); 
+    box.setAttribute('style', `border: 1px solid #dddddd; width: ${Math.round(400/sliderValue) - 2}px ; height: ${Math.round(400/sliderValue) - 2}px;`);
+    grid.appendChild(box);
+}
+
+// Edit grid using slider
+
+let slider = document.getElementById("dimensions");
+
+slider.addEventListener('change', (e) => {
+
+    //remove current grid
+    let g = document.getElementById("grid");
+
+    while (g.hasChildNodes()) {
+        g.removeChild(g.firstChild);
+    }
+    
+    //create new grid
+    sliderValue = e.target.value;
+
+    console.log(sliderValue);
+
+    gridColumn = 400/sliderValue - 1;
+    gridColumn = gridColumn.toFixed(7);
+
+    grid.setAttribute('style', `display: grid; grid-template-columns: repeat(${sliderValue} , 1fr); `); 
+
+    for(let i=0; i<(sliderValue*sliderValue); i++) {
+    let box = document.createElement('div');
+
+    box.classList.add('box');
+    let boxDimensions = 400/sliderValue - 2;
+    boxDimensions = boxDimensions.toFixed(7); 
+    box.setAttribute('style', `border: 1px solid #dddddd; padding:0; margin:0; 1px solid black; width: ${boxDimensions}px ; height: ${boxDimensions}px;`);
+    grid.appendChild(box);
+    }
+
+    gridSize.textContent = `${sliderValue} x ${sliderValue}`
+
+        
+});
+
+
+// window.onload = createGrid();
+
+
+
 
 
 
@@ -24,17 +83,10 @@ grid.setAttribute('style', `display: grid; width:400px ; height: 400px; grid-tem
 /*
 Set up intial grid
 - event listener for mousever is made 
-- if amouse passes over box its bckground is changed to orange
+- if a mouse passes over box its bckground is changed to orange
 */
 
-for(let i=0; i<(slider*slider); i++) {
-    let box = document.createElement('div');
 
-    box.classList.add('box'); 
-    box.setAttribute('style', `border: 1px solid black; width: ${Math.round(400/slider) - 2}px ; height: ${Math.round(400/slider) - 2}px;`);
-    grid.appendChild(box);
-}
-container.appendChild(grid);
 
 /* Orange Pen 
 - Set up function for orange ped
@@ -55,11 +107,6 @@ mouseover if variable is true continue to change background
 
 on mouse up set variable to false
 false = no background changing
-
-
-function(e) {
-            e.target.style.backgroundColor = "orange";
-        }
 
 */
 
@@ -88,6 +135,42 @@ function orangePen() {
 }
 
 
+/* Rainbow Pen
+- randomly choose colours
+
+*/
+
+const rainbow_pen = document.querySelector('#rainbow_pen');
+rainbow_pen.addEventListener("click", () => rainbowPen());
+
+function randomColor() {
+    
+    let random_color = Math.floor(Math.random()*16777215).toString(16);
+    return random_color;
+}
+
+function rainbowPen() {
+
+    let isDrawing = false;
+    let boxes = document.querySelectorAll(".box");
+
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].addEventListener("mousedown", (e) => {
+        e.target.style.backgroundColor = `#${randomColor()}`;
+        isDrawing = true; });
+        boxes[i].addEventListener("mouseover", (e) => {
+            if (isDrawing == true) {
+            e.target.style.backgroundColor = `#${randomColor()}`;
+            }
+        });
+        boxes[i].addEventListener("mouseup", () => isDrawing = false );
+    }
+
+
+}
+
+
+
 /*
 Clear All
 - Clear all button is selected
@@ -103,6 +186,8 @@ function clearAll() {
     for (let i = 0; i < boxes.length; i++) {
       boxes[i].style.backgroundColor = "white";
     }
+
+    
 }    
 
 
